4b478efda1446d5b28d7ecdbe1bcc962138a8580
Yu-Ming Chang <yumin686@andestech.com>
2023-08-02 14:07:54 +0800

    target/riscv: Add mecc_code csr support for NX45V. Signed-off-by: Yu-Ming Chang <yumin686@andestech.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -77,6 +77,7 @@ typedef struct CPUArchState CPURISCVState;
 #define RVJ RV('J')
 #define RVG RV('G')
 #define RVB RV('B')
+#define RVX RV('X')
 
 extern const uint32_t misa_bits[];
 const char *riscv_get_misa_ext_name(uint32_t bit);
