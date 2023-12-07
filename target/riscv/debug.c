3fb826f5328a2b9f54297f7657539ef79ac8d402
Alvin Chang <alvinga@andestech.com>
2023-12-07 16:37:15 +0800

    target/riscv: Add definitions for trigger mode fields
    Signed-off-by: Alvin Chang <alvinga@andestech.com>

--- target/riscv/debug.c
+++ target/riscv/debug.c
@@ -343,19 +343,19 @@ static bool trigger_priv_match(CPURISCVState *env, trigger_type_t type,
             return false;
         }
         /* check U/S/M bit against current privilege level */
-        if ((ctrl >> 3) & BIT(env->priv)) {
+        if ((ctrl >> TYPE2_U_SHIFT) & BIT(env->priv)) {
             return true;
         }
         break;
     case TRIGGER_TYPE_AD_MATCH6:
         if (env->virt_enabled) {
             /* check VU/VS bit against current privilege level */
-            if ((ctrl >> 23) & BIT(env->priv)) {
+            if ((ctrl >> TYPE6_VU_SHIFT) & BIT(env->priv)) {
                 return true;
             }
         } else {
             /* check U/S/M bit against current privilege level */
-            if ((ctrl >> 3) & BIT(env->priv)) {
+            if ((ctrl >> TYPE6_U_SHIFT) & BIT(env->priv)) {
                 return true;
             }
         }
@@ -363,12 +363,12 @@ static bool trigger_priv_match(CPURISCVState *env, trigger_type_t type,
     case TRIGGER_TYPE_INST_CNT:
         if (env->virt_enabled) {
             /* check VU/VS bit against current privilege level */
-            if ((ctrl >> 25) & BIT(env->priv)) {
+            if ((ctrl >> ITRIGGER_VU_SHIFT) & BIT(env->priv)) {
                 return true;
             }
         } else {
             /* check U/S/M bit against current privilege level */
-            if ((ctrl >> 6) & BIT(env->priv)) {
+            if ((ctrl >> ITRIGGER_U_SHIFT) & BIT(env->priv)) {
                 return true;
             }
         }
@@ -390,54 +390,11 @@ static bool trigger_priv_match(CPURISCVState *env, trigger_type_t type,
     return false;
 }
 
-static bool trigger_textra_match(CPURISCVState *env, trigger_type_t type,
-                                 int trigger_index)
-{
-    target_ulong textra = env->tdata3[trigger_index];
-    target_ulong mhvalue, mhselect;
-
-    if (type < TRIGGER_TYPE_AD_MATCH || type > TRIGGER_TYPE_AD_MATCH6) {
-        /* textra checking is only applicable when type is 2, 3, 4, 5, or 6 */
-        return true;
-    }
-
-    switch (riscv_cpu_mxl(env)) {
-    case MXL_RV32:
-        mhvalue  = get_field(textra, TEXTRA32_MHVALUE);
-        mhselect = get_field(textra, TEXTRA32_MHSELECT);
-        break;
-    case MXL_RV64:
-    case MXL_RV128:
-        mhvalue  = get_field(textra, TEXTRA64_MHVALUE);
-        mhselect = get_field(textra, TEXTRA64_MHSELECT);
-        break;
-    default:
-        g_assert_not_reached();
-    }
-
-    /* Check mhvalue and mhselect. */
-    switch (mhselect) {
-    case MHSELECT_IGNORE:
-        break;
-    case MHSELECT_MCONTEXT:
-        /* Match if the low bits of mcontext/hcontext equal mhvalue. */
-        if (mhvalue != env->mcontext) {
-            return false;
-        }
-        break;
-    default:
-        break;
-    }
-
-    return true;
-}
-
 /* Common matching conditions for all types of the triggers. */
 static bool trigger_common_match(CPURISCVState *env, trigger_type_t type,
                                  int trigger_index)
 {
-    return trigger_priv_match(env, type, trigger_index) &&
-           trigger_textra_match(env, type, trigger_index);
+    return trigger_priv_match(env, type, trigger_index);
 }
 
 /* type 2 trigger */
