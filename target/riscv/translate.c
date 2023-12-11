8867f1892ea63b511cf445fa8ea7836a96214550
Yu-Ming Chang <yumin686@andestech.com>
2023-12-11 13:52:06 +0800

    target/riscv: Add support of HSP(Hardware Stack Protection and recording). (#89)
    Signed-off-by: Yu-Ming Chang <yumin686@andestech.com>
    Reviewed-on: https://gitea.andestech.com/RD-SW/qemu/pulls/89
    Reviewed-by: Ethan Zhan-Yang Chen <ethan84@andestech.com>
    Reviewed-by: Alvin Che-Chia Chang <alvinga@andestech.com>
    Co-authored-by: Yu-Ming Chang <yumin686@andestech.com>
    Co-committed-by: Yu-Ming Chang <yumin686@andestech.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -389,6 +389,12 @@ static TCGv dest_gprh(DisasContext *ctx, int reg_num)
 
 static void gen_set_gpr(DisasContext *ctx, int reg_num, TCGv t)
 {
+    if (ctx->cfg_ptr->ext_XAndesV5Ops == true) {
+        if (reg_num == xSP) {
+            gen_helper_andes_v5_hsp_check(tcg_env, t);
+        }
+    }
+
     if (reg_num != 0) {
         switch (get_ol(ctx)) {
         case MXL_RV32:
@@ -410,6 +416,12 @@ static void gen_set_gpr(DisasContext *ctx, int reg_num, TCGv t)
 
 static void gen_set_gpri(DisasContext *ctx, int reg_num, target_long imm)
 {
+    if (ctx->cfg_ptr->ext_XAndesV5Ops == true) {
+        if (reg_num == xSP) {
+            gen_helper_andes_v5_hsp_check(tcg_env, tcg_constant_tl(imm));
+        }
+    }
+
     if (reg_num != 0) {
         switch (get_ol(ctx)) {
         case MXL_RV32:
